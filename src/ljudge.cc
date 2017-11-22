ae61256b2ac22bce84c18a24e7b871c0ab86f198
Richard <richard9372@gmail.com>
2017-11-22 18:08:30 +0800

    Add default env

--- src/ljudge.cc
+++ src/ljudge.cc
@@ -213,6 +213,8 @@ struct LrunArgs : public vector<string> {
     append("--network", "false");
     append("--chdir", "/tmp");
     append("--env", "ONLINE_JUDGE", "1");
+    append("--env", "LANG", "en_US.UTF-8");
+    append("--env", "LC_ALL", "en_US.UTF-8");
     append("--env", "HOME", "/tmp");
     append("--env", "PATH", "/usr/bin:/bin:/etc/alternatives:/usr/local/bin");
     // Pass as-is
